33a89d0843fddaf75951329241ceaf5b8b746dd5
Alan Hornbaker <alanhornbaker@gmail.com>
2022-12-22 21:31:41 -0600

    developed psuedo-code for initial key event

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,16 @@
 
 //Here I'll be writing the code for the function that captures the users
 //guess as a key event
+
+
+
+//Psuedo-Code:
+
+//addEVentListener for keyEvent
+    //should only be accepting alphabetical inputs
+    //should only accept letters that haven't been guessed already
+    //
+
+//data from the keyevent to be captured in localStorage as user guess
+//user guess should be storage as a var or whatever, to make it easier
+//to call on and use later on.
